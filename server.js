3a3ffd98d15014d453834517a692cd1e91c6e756
tomaskrug <tomi.krug@gmail.com>
2015-10-19 15:25:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.post('/event/search', function(req, res) {
   });
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(8080, function() {
   var host = server.address().address;
   var port = server.address().port;
 
